cff9563a174ac30aa96d3cda51cd8aa6720b5b7e
girish <girish.h@insuide.com>
2025-05-05 17:09:57 +0530

    feat: switch to HashRouter for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import FormListView from './pages/FormListView';
 import FormAWrapper from './forms/formA/FormAWrapper';
 import FormBWrapper from './forms/formB/FormBWrapper';
@@ -9,8 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<FormListView />} />
-        <Route path="/formA" element={<FormAWrapper />} />
-        <Route path="/formB" element={<FormBWrapper />} />
+        <Route path="formA" element={<FormAWrapper />} />
+        <Route path="formB" element={<FormBWrapper />} />
       </Routes>
     </Router>
   );
